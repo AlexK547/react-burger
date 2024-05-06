e3695c11ab82c20e05035945f9d3e91e4a736931
Alex Kozhenov <kozhenov1984@mail.ru>
2024-05-06 11:10:58 +0400

    add connect to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
-import './App.css';
-import AppHeader from './components/app-header/app-header';
-import BurgerConstructor from './components/burger-constructor/burger-constructor';
-import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
-import { data } from "./utils/data";
+import { useEffect, useState } from "react";
+import "./App.css";
+import AppHeader from "./components/app-header/app-header";
+import BurgerConstructor from "./components/burger-constructor/burger-constructor";
+import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
+// import { data } from "./utils/data";
+
+const URLAddress = "https://norma.nomoreparties.space/api/ingredients";
 
 function App() {
+  const [data, setData] = useState({
+    productData: [],
+    loading: false,
+    hasError: false,
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      setData({ ...data, loading: true, hasError: false });
+      fetch(URLAddress)
+        .then((res) => res.json())
+        .then((dataList) => {
+          setData({
+            ...data,
+            productData: dataList.data,
+            loading: false,
+            hasError: false,
+          });
+        })
+        .catch((e) => {
+          setData({ ...data, loading: false, hasError: true });
+          alert("Error!");
+        });
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <AppHeader />
-      <main className='main'>
-        <BurgerIngredients listData={data}/>
-        <BurgerConstructor listData={data}/>
+      <main className="main">
+        {!data.loading && !data.hasError && data.productData.length && (
+          <>
+            <BurgerIngredients listData={data.productData} />
+            <BurgerConstructor listData={data.productData} />
+          </>
+        )}
       </main>
     </div>
   );
